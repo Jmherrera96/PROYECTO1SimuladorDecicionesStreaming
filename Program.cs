fce5211e5c6bb087111614cbfad58d2a8585eee8
Jmherrera96 <JMHERRERAD@correo.url.edu.gt>
2026-03-24 21:43:47 -0600

    Metodo para mostar reglas

--- Program.cs
+++ Program.cs
@@ -20,10 +20,11 @@ class SimuladorStreaming
             {
                 case 1:
                     Console.WriteLine("--- INGRESAR CONTENIDO ---");
-                    // Aquí iremos agregando la entrada de datos
+                    // Entrada de datos
                     break;
                 case 2:
-                    // Llamará a MostrarReglas (próximo commit)
+                    // Mostrar reglas
+                    MostrarReglas();
                     break;
                 case 3:
                     // Mostrar estadísticas
@@ -39,5 +40,30 @@ class SimuladorStreaming
                     break;
             }
         } while (menu != 5);
+    }
+    static void MostrarReglas()
+    {
+        Console.WriteLine("= REGLAS DEL SISTEMA =");
+        Console.WriteLine("HORARIO PERMITIDO:");
+        Console.WriteLine("  Todo publico : cualquier hora");
+        Console.WriteLine("  +13          : entre 6 y 22 horas");
+        Console.WriteLine("  +18          : entre 22 y 5 horas");
+        Console.WriteLine("DURACION POR TIPO:");
+        Console.WriteLine("  Pelicula       : 60 a 180 min");
+        Console.WriteLine("  Serie          : 20 a 90 min");
+        Console.WriteLine("  Documental     : 30 a 120 min");
+        Console.WriteLine("  Evento en vivo : 30 a 240 min");
+        Console.WriteLine("PRODUCCION:");
+        Console.WriteLine("  Baja         : solo Todo publico o +13");
+        Console.WriteLine("  Media o Alta : cualquier clasificacion");
+        Console.WriteLine("IMPACTO:");
+        Console.WriteLine("  Alto   : prod. alta, duracion > 120 min, o entre 20-23 h");
+        Console.WriteLine("  Medio  : prod. media o duracion entre 60-120 min");
+        Console.WriteLine("  Bajo   : prod. baja y duracion menor a 60 min");
+        Console.WriteLine("DECISION:");
+        Console.WriteLine("  Publicar           : impacto bajo o medio sin limites");
+        Console.WriteLine("  Publicar c/ajustes : impacto bajo/medio en valor limite");
+        Console.WriteLine("  Enviar a revision  : impacto alto");
+        Console.WriteLine("  Rechazar           : incumple regla obligatoria");
     }
 }
